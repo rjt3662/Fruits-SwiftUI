7dc54e11c1e3cf6a6b226e97e4241793b5db7e0b
rjt3662 <rajat.m324@gmail.com>
2021-12-30 16:01:53 +0530

    Base Setup

---
+++ Fruits/App/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Rajat Mishra on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ Fruits/App/OnboardingView.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingView.swift
+//  Fruits
+//
+//  Created by Rajat Mishra on 30/12/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
